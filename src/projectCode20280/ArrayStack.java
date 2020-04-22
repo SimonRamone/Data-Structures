88316c95d017e2057effd04bd846d04d300b5681
SimonRamone <sramonas@yahoo.com>
2020-04-22 14:38:41 +0300

    ArrayStack implemented.

--- src/projectCode20280/ArrayStack.java
+++ src/projectCode20280/ArrayStack.java
@@ -2,39 +2,87 @@ package projectCode20280;
 
 public class ArrayStack<E> implements Stack<E> {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static final int N = 1000; 	//array capacity
+	private E[] data;					//generic array used for storage
+	private int t = -1;					//index of the top element in stack
+	
+	public ArrayStack() {
+		this(N); 						//constructs stack with default capacity
+	}
+	
+	@SuppressWarnings("unchecked")
+	public ArrayStack(int n) {			//constructs stack with given capacity
+		data = (E[]) new Object[n];		//safe cast
+	}
 
+	public static void main(String[] args) {
+		ArrayStack<Integer> S = new ArrayStack<Integer>();
+		System.out.println(S.isEmpty());
+		S.push(1);
+		System.out.println(S);
+		System.out.println(S.isEmpty());
+		S.push(2);
+		S.push(3);
+		System.out.println(S);
+		System.out.println(S.size());
+		S.push(4);
+		S.push(5);
+		System.out.println(S);
+		System.out.println(S.pop());
+		System.out.println(S);
+		S.pop();
+		S.pop();
+		System.out.println(S);
+		System.out.println(S.top());
+		S.pop();
+		S.pop();
+		System.out.println(S.isEmpty());
+		System.out.println(S.size());
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return t + 1;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return t == -1;
 	}
 
 	@Override
 	public void push(E e) {
-		// TODO Auto-generated method stub
-		
+		if(size() == N) System.out.println("Stack is full.");
+		else{
+			t++;
+			data[t] = e;
+		}
 	}
 
 	@Override
 	public E top() {
-		// TODO Auto-generated method stub
-		return null;
+		if(isEmpty()) return null;
+		else return data[t];
 	}
 
 	@Override
 	public E pop() {
-		// TODO Auto-generated method stub
-		return null;
+		if(isEmpty()) return null;
+		else {
+			E valueAtTopOfStack = data[t];
+			t--;
+			return valueAtTopOfStack;
+		}
+	}
+	
+	public String toString() {
+		String str = "";
+		int index = t;
+		while(index >= 0) {
+			str += data[index] + " ";
+			index--;
+		}
+		return str;
 	}
 
 }
